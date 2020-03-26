8015b9eb88cc55f21c8ab421589e7a4c83f716a3
H. Yusuf Usta <46392667+Quiec@users.noreply.github.com>
2020-03-26 21:02:02 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,17 @@
-BOTTOKEN = ""
-APIID = 8
-APIHASH = "7245de8e747a0d6fbe11f7cc14fcc0bb"
+import os
+
+BOTTOKEN = os.environ.get("BOTTOKEN", "")
+APIID = int(os.environ.get("APIID", 6))
+APIHASH = os.environ.get("APIHASH", "eb06d4abfb49dc3eeb1aeb98ae0f581e")
+DOWNLOADPATH = os.environ.get("DOWNLOADPATH", "Downloads/")
+USERNAME = "@URLYUKLEBOT"
+
+"""
+If you using VPS, edit like this:
+
+BOTTOKEN = "123456:xxxx"
+APIID = 6
+APIHASH = eb06d4abfb49dc3eeb1aeb98ae0f581e
 DOWNLOADPATH = "Downloads/"
 USERNAME = "@URLYUKLEBOT"
+"""
