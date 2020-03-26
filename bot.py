a8ca40267cfdc6dfb966250205b9248a4c2b891b
H. Yusuf Usta <46392667+Quiec@users.noreply.github.com>
2020-03-26 20:47:54 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,13 +1,14 @@
 from telethon import TelegramClient, events
-from download_from_url import download_file, get_size
+from download_from_url import download_file
 from config import BOTTOKEN, APIID, APIHASH, DOWNLOADPATH, USERNAME
 import os
 import time
 import datetime
 import asyncio
 import aiohttp
-from utils import progress, humanbytes, time_formatter
+from utils import progress, humanbytes, time_formatter, convert_from_bytes
 import traceback
+import speedtest
 
 bot = TelegramClient('upbot', APIID, APIHASH).start(bot_token=BOTTOKEN)
 
@@ -23,7 +24,7 @@ def get_date_in_two_weeks():
 async def send_to_transfersh_async(file):
     
     size = os.path.getsize(file)
-    size_of_file = get_size(size)
+    size_of_file = humanbytes(size)
     final_date = get_date_in_two_weeks()
     file_name = os.path.basename(file)
 
@@ -37,6 +38,16 @@ async def send_to_transfersh_async(file):
                     
     print("Link to download file(will be saved till {}):\n{}".format(final_date, download_link))
     return download_link, final_date, size_of_file
+
+async def send_to_tmp_async(file):
+    url = 'https://tmp.ninja/api.php?d=upload-tool'
+    
+    with open(file, 'rb') as f:
+            async with aiohttp.ClientSession() as session:
+                async with session.post(url, data={"file": f}) as response:
+                    download_link =  await response.text()
+                    
+    return download_link
 
 
 @bot.on(events.NewMessage(pattern='/start'))
@@ -69,7 +80,7 @@ async def up(event):
                 ))
 
             zaman = str(time.time() - start)
-            await bot.send_file(event.chat.id, dosya, caption=f"{filename} uploaded in {zaman} seconds! By {USERNAME}")
+            await bot.send_file(event.chat.id, dosya, force_document=True, caption=f"{filename} uploaded in {zaman} seconds! By {USERNAME}")
         except Exception as e:
             traceback.print_exc()
 
@@ -109,6 +120,63 @@ async def tsh(event):
             await event.respond(f"Uploading Failed\n\n**Error:** {e}")
 
     raise events.StopPropagation
+
+@bot.on(events.NewMessage(pattern='/speedtest'))
+async def speedtest(event):
+    ilk = await event.respond(f"Calculating my internet speed. Please wait!")
+    start = time.time()
+    s = speedtest.Speedtest()
+    s.get_best_server()
+    s.download()
+    s.upload()
+    end = time.time()
+    ms = (end - start).microseconds / 1000
+    response = s.results.dict()
+    download_speed = response.get("download")
+    upload_speed = response.get("upload")
+    ping_time = response.get("ping")
+    client_infos = response.get("client")
+    i_s_p = client_infos.get("isp")
+    i_s_p_rating = client_infos.get("isprating")
+    await ilk.edit("""**SpeedTest** completed in {} seconds
+        Download: {}
+        Upload: {}
+        Ping: {}
+        Internet Service Provider: {}
+        ISP Rating: {}""".format(ms, convert_from_bytes(download_speed), convert_from_bytes(upload_speed), ping_time, i_s_p, i_s_p_rating))
+
+    raise events.StopPropagation
+@bot.on(events.NewMessage(pattern='/tmpninja'))
+async def tmp(event):
+    if event.reply_to_msg_id:
+        start = time.time()
+        url = await event.get_reply_message()
+        ilk = await event.respond("Downloading...")
+        try:
+            file_path = await url.download_media(progress_callback=lambda d, t: asyncio.get_event_loop().create_task(
+                    progress(d, t, ilk, start, "Downloading...")
+                ))
+        except Exception as e:
+            traceback.print_exc()
+            print(e)
+            await event.respond(f"Downloading Failed\n\n**Error:** {e}")
+        
+        await ilk.delete()
+
+        try:
+            orta = await event.respond("Uploading to TmpNinja...")
+            download_link = await send_to_tmp_async(file_path)
+
+            zaman = str(time.time() - start)
+            await orta.edit(f"File Successfully Uploaded to TmpNinja.\n\nLink:{download_link}")
+        except Exception as e:
+            traceback.print_exc()
+            print(e)
+            await event.respond(f"Uploading Failed\n\n**Error:** {e}")
+
+    raise events.StopPropagation
+
+
 
 def main():
     if not os.path.isdir(DOWNLOADPATH):
